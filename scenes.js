3d24491ebcd2ca10f1b2cb0a69801aefd0e655f5
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2018-08-24 15:04:28 -0400

    Add scenes.js and turtles.js

---
+++ scenes.js
@@ -0,0 +1,9 @@
+let Scene = function(t, s, z) {
+    this.turtle = t;
+    this.seed = s;
+    this.zoom = z;
+};
+
+let firstScene = new Scene();
+
+let scene = firstScene;
---
+++ turtles.js
@@ -0,0 +1,1 @@
+let firstTurtle = new Turtle();
